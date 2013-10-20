1a8936443d5b3a6291b387f630d36ed713740202
Makoto Isawa <makoto.isawa@gmail.com>
2013-10-20 11:27:51 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Match::Application.config.secret_token = '3273e4f818e00df34ae8b60a9c889ef5cdae44f4c4bca43d6576e9045438989f415c7dde872845fa26467bcd5a6ebb5d604bbd69f0dfcc8ec5655c8dee8c648d'
